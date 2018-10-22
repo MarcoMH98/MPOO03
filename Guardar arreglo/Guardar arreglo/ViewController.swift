f041d96e2fc9539fabc3bbe3748d541c30a01802
Marco Antonio Martínez Hernández <drake.markos@gmail.com>
2018-10-22 15:10:22 -0500

    Add files via upload

---
+++ Guardar arreglo/Guardar arreglo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Guardar arreglo
+//
+//  Created by Macbook on 01/10/18.
+//  Copyright © 2018 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func Guardar(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var Caja: UITextField!
+    
+    
+   
+
+    
+
+}
+
