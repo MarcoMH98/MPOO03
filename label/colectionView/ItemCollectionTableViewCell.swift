bc60392fca9035820e968a87ffb2ae0054f3ff89
Marco Antonio Martínez Hernández <drake.markos@gmail.com>
2018-10-22 15:14:17 -0500

    Add files via upload

---
+++ label/colectionView/ItemCollectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCollectionTableViewCell.swift
+//  colectionView
+//
+//  Created by Macbook on 19/09/18.
+//  Copyright © 2018 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
