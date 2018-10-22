a97db19ccac431e9ca890fe305d7917f9101f077
Marco Antonio Martínez Hernández <drake.markos@gmail.com>
2018-10-22 15:09:24 -0500

    Add files via upload

---
+++ itunes 2/itunes/itemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  itemCollectionViewCell.swift
+//  itunes
+//
+//  Created by Macbook on 26/09/18.
+//  Copyright © 2018 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class itemCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var Precio: UILabel!
+    
+    @IBOutlet weak var portada: UIImageView!
+    
+    @IBOutlet weak var titulo: UILabel!
+}
